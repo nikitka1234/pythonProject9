9d159a8f9c45136dccda1e16440dab34085f296f
Nikita Samsonenkov <mrkimatsuki@gmail.com>
2023-05-02 16:41:35 +0500

    Commit #2

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, EmailField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Optional
+
+
+class FeedbackForm(FlaskForm):
+    name = StringField("Имя", validators=[DataRequired(message='Поле "Имя" не моежт быть пустым')])
+    text = TextAreaField("Отзыв", validators=[DataRequired(message='Поле "Отзыв" не моежт быть пустым')])
+    email = EmailField("Почта", validators=[Optional()])
+    rating = SelectField("Оценка", choices=[5, 4, 3, 2, 1], default=5)
+    submit = SubmitField("Отправить")
 
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "SECRET_KEY"
 
 content = [
     {"title": "Название для шаблона из словаря", "text": "Текст для шаблона из словаря"},
@@ -18,6 +31,22 @@ def news():
     return "Новости"
 
 
+def feedback():
+    form = FeedbackForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        text = form.text.data
+        email = form.email.data
+        rating = form.rating.data
+
+        print(name, text, email, rating, sep="\n\n---------------------\n\n")
+
+        return redirect(url_for("index"))
+
+    return render_template("feedback.html", form=form)
+
+
 def news_detail(id):
     return render_template("news_detail.html", **content[id])
 
@@ -28,5 +57,6 @@ def category(name):
 
 app.add_url_rule("/", "index", index)
 app.add_url_rule("/news", "news", news)
+app.add_url_rule("/feedback", "feedback", feedback, methods=["GET", "POST"])
 app.add_url_rule("/news_detail/<int:id>", "news_detail", news_detail)
 app.add_url_rule("/category/<string:name>", "category", category)
